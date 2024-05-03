f6ae57cb6fedd9411d8a38243c86a9f00967cd69
Clazariuk1 <Clazariuk@gmail.com>
2024-05-03 14:08:27 -0700

    events and fromlast review

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,24 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    // initialize two variables that will serve as our reference points
+    let slow = list.getFirst()
+    let fast = list.getFirst()
+
+    // create a while loop that iterates each value point of 'n'; this loop moves the 'fast' variable forward on the list with each iteration.
+    while (n > 0) {
+        fast = fast.next
+        n--
+    }
+    // create ANOTHER while loop that references the new placement of 'fast' after the first loop. The loop runs until the fast variable hits the final node in the list, organically discovering the end of the list.
+    while (fast.next) {
+        // from start and n distance apart from fast, the slow variable ALSO begins moving forward on the list until fast hits the end. Because the gap between slow and fast is n nodes long, the position of slow will be the requested node from the function.
+        slow = slow.next
+        fast = fast.next
+    }
+    // return the slow variable answer
+    return slow
+}
 
 module.exports = fromLast;
