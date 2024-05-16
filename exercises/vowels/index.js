ab580fa1426cc9d8b8d08525b17903bd0eebbcfb
Clazariuk1 <Clazariuk@gmail.com>
2024-05-16 15:20:15 -0600

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,19 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const VOWELS = 'aeiou'
+    let answer = 0
+    for (let i = 0; i < str.length; i++) {
+        if (VOWELS.includes(str[i].toLowerCase())) {
+            answer++
+        }
+    }
+    return answer
+}
 
 module.exports = vowels;
+
+console.log(vowels('Hi There!')) // 3
+console.log(vowels('Why do you ask?')) // 4
+console.log(vowels('Why?')) // 0
